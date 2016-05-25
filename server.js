23cd87edc4c4a06415adfd63e68e2edc6d74191b
Mousbah El Fil <quantummousbah@gmail.com>
2016-05-25 18:31:08 +0300

    adding extra files

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ var express = require("express");
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.get("/contactlist", function (req, res) {
+    console.log("Received a GET request");
+    person1 = {
+        name: "M",
+        email: "M@gmail.com",
+        number: "111111"
+    }
 
+    person2 = {
+        name: "s",
+        email: "s@gmail.com",
+        number: "222"
+    }
+    person3 = {
+        name: "d",
+        email: "d@gmail.com",
+        number: "11212324546"
+    }
+
+    var contactlist = [person1, person2, person3];
+    res.json(contactlist);
+})
 app.listen(3000);
 console.log("server running on port 3000")
